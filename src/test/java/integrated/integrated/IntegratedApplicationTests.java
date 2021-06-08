b94e8db8ce7d00a5d6da29dba2a004ca89847341
submarine.kim <submarine.kim@kakaocorp.com>
2021-06-08 21:19:10 +0900

    restAPI 생성

---
+++ src/test/java/integrated/integrated/IntegratedApplicationTests.java
@@ -0,0 +1,13 @@
+package integrated.integrated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
